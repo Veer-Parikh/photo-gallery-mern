a5345aa1c1a3071d6c0c00cf402c0f16cf137f8c
Veer-Parikh <parikhveer71@gmail.com>
2024-03-31 12:14:18 +0530

    minor changes modal added

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Grid = ({ photos }) => {
+const Grid = ({ photos, handleDelete }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleClick = (id) => {
+    setSelectedImage(id);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div>
       <h1>Gallery</h1>
@@ -10,12 +20,20 @@ const Grid = ({ photos }) => {
             <img
               src={`http://localhost:3000/uploads/${photo}`}
               alt="grid_image"
+              onClick={() => handleClick(_id)} // Open image in modal on click
             />
+            <button onClick={() => handleDelete(_id)} className="delete_button">Delete</button>
           </div>
         ))}
       </div>
+      {selectedImage && (
+        <div className="modal">
+          <span className="close" onClick={handleCloseModal}>&times;</span>
+          <img className="modal-content" src={`http://localhost:3000/uploads/${photos.find(p => p._id === selectedImage).photo}`} alt="modal_image" />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
